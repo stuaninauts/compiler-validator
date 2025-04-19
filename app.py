6147e5dd3c16247d427e85a7d50e997272e1a438
Eduardo Stuani <eduardostuani.cs@gmail.com>
2025-04-19 11:55:24 -0300

    Add references links

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from dataclasses import dataclass
 import subprocess
 import shutil
 
+# TODO: comentado por enquanto (necessita de permissão de escrita no servidor)
 # criacao dos diretorios que serao feitos os uploads
-UPLOADS_DIR = Path("uploads")
-UPLOADS_DIR.mkdir(exist_ok=True)
+UPLOADS_DIR = Path(".")
+# UPLOADS_DIR.mkdir(exist_ok=True)
 
-# ====================================
+# ====================================z
 # AUXILIAR FUNCTIONS
 # ====================================
 
@@ -22,6 +23,8 @@ class EvaluateResult:
     testes_ok: bool
     mensagem_estrutura: str
     mensagem_testes: str
+    etapa: str = None
+    nome_arquivo: str = None
 
 def evaluate(file_dict: dict, etapa: str, unique_dir: str) -> EvaluateResult:
     """Executa a validação do arquivo submetido."""
@@ -32,8 +35,10 @@ def evaluate(file_dict: dict, etapa: str, unique_dir: str) -> EvaluateResult:
         if file_dict['name'] != f'{etapa}.tgz':
             return False
         
-        extract_all_files(file_dict['datapath'], unique_dir)
-
+        extract_succedd = extract_all_files(file_dict['datapath'], unique_dir)
+        if not extract_succedd:
+            return False
+        
         # depois de extraido testa se makefile exisate e esta na raiz
         makefile_path_lower = unique_dir / 'makefile'
         makefile_path_upper = unique_dir / 'Makefile'
@@ -69,12 +74,16 @@ def evaluate(file_dict: dict, etapa: str, unique_dir: str) -> EvaluateResult:
         else "Testes: Not OK!"
     )
 
-    return EvaluateResult(estrutura_ok, testes_ok, msg_estrutura, msg_testes)
+    return EvaluateResult(estrutura_ok, testes_ok, msg_estrutura, msg_testes, etapa, file_dict['name'])
 
 def extract_all_files(tar_file_path, extract_to):
     """Extrai o arquivo .tgz para o diretório especificado"""
-    with tarfile.open(tar_file_path, 'r') as tar:
-        tar.extractall(extract_to)
+    try:
+        with tarfile.open(tar_file_path, 'r') as tar:
+            tar.extractall(extract_to)
+        return True
+    except Exception as e:
+        return False
 
 def remove_file_from_disk(file_dict: dict, extracted_dir: str):
     """Remove arquivos do disco após a validação"""
@@ -83,15 +92,16 @@ def remove_file_from_disk(file_dict: dict, extracted_dir: str):
 
 def feedback_message(text: str, type: str) -> ui.Tag:
     """Gera mensagem de feedback na UI"""
-    cores = {
-        "success": "green",
-        "error": "red",
-        "warning": "gray"
+    colors = {
+        "success": ("green", "white"),
+        "error": ("red", "white"),
+        "warning": ("gray", "white"),
+        "info": ("white", "black")
     }
-    cor = cores.get(type)
+    bg_color, text_color = colors.get(type, ("white", "black"))
     return ui.tags.div(
         text,
-        style=f"background-color: {cor}; color: white; margin: 10px 0; padding: 10px;"
+        style=f"background-color: {bg_color}; color: {text_color}; margin: 10px 0; padding: 10px;"
     )
 
 # ====================================
@@ -105,11 +115,49 @@ app_ui = ui.page_fluid(
         "Selecione uma etapa:",
         {f"etapa{i}": f"Etapa {i}" for i in range(1, 7)},
     ),
-    ui.input_file("file_upload", "Faça upload do seu entregável:", accept=[".tgz"]),
-    ui.input_action_button("submit_button", "Enviar Submissão"),
+    ui.input_file(
+        "file_upload",
+        "Faça upload do seu entregável:",
+        accept=[".tgz"],
+    ),
+    ui.tooltip(
+        ui.input_action_button(
+            "submit_button",
+            "Enviar Submissão",
+        ),
+        "teste",
+        placement="right",
+        ),
+    ui.output_ui("div_submission_info"),
     ui.output_ui("div_file_structure"),
     ui.output_ui("div_all_tests"),
+    ui.tags.div(
+        [
+            ui.hr(style="margin-bottom: 1.5em; margin-left: 0;"),
+            ui.tags.p(
+                ui.tags.img(
+                    src="https://img.icons8.com/ios-filled/50/000000/scales.png",
+                    style="width: 20px; height: 20px; vertical-align: middle; margin-right: 5px;"
+                ),
+                "Este projeto é de código aberto sob a ",
+                ui.tags.a("Licença MIT", href="https://opensource.org/licenses/MIT", target="_blank")
+            ),
+            ui.tags.p(
+                ui.tags.a(
+                    ui.tags.img(
+                        src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+                        style="width: 20px; height: 20px; vertical-align: middle; margin-right: 5px;"
+                    ),
+                    "Acesse o repositório no GitHub",
+                    href="https://github.com/stuaninauts/compiler-validator",
+                    target="_blank"
+                )
+            )
+        ],
+        style="position: fixed; bottom: 0; width: 100%; font-size: 0.9em; color: gray; margin-bottom: 10px"
+    )
 )
+
 # ====================================
 # SERVER
 # ====================================
@@ -119,6 +167,7 @@ def server(input, output, session):
     def result():
         """Quando botão de Submit é pressionado salva arquivo no disco e realiza a validação"""
         # TODO: fix submit button pressed 2 times
+        # try:
         file_info = input.file_upload()
         if not file_info:
             return None
@@ -131,9 +180,23 @@ def server(input, output, session):
         print(f"file_dict: {file_dict}")
         print(f"Diretorio gerado: {unique_dir}")
         print(f"Etapa selecionada: {etapa}")
-
+        
         return evaluate_result
+        # except Exception as e:
+        #     print(f"Erro ao processar o arquivo: {e}")
+        #     return None
 
+
+    @output
+    @render.ui
+    def div_submission_info():
+        """Div relacionada ao feedback do envio"""
+        res = result()
+        if res is None:
+            return ""
+        type = "info"
+        return feedback_message(f"Etapa: {res.etapa}, Arquivo: {res.nome_arquivo}", type)
+    
     @output
     @render.ui
     def div_file_structure():
